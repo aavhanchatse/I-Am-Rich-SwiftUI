5b5f5db8423e297604e90b412b2f85ea15c0f847
Aavhan Chatse <chatseaavhan@gmail.com>
2024-09-30 10:59:05 +0530

    Initial Commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Aavhan Chatse on 30/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
